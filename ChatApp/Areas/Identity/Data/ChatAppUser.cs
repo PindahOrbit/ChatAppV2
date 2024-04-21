92f43f140ea9099531dbb60878c8b035f7cde749
Leeroy Mubaiwa <45221940+Leeroy-Mubaiwa@users.noreply.github.com>
2024-04-21 08:16:50 +0200

    Add project files.

---
+++ ChatApp/Areas/Identity/Data/ChatAppUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ChatApp.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ChatAppUser class
+public class ChatAppUser : IdentityUser
+{
+    [Column("full_name")]
+    public string FullName { get; set; }
+}
+
